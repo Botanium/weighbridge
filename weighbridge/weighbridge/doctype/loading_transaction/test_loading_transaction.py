8e4828992181a8aa42f33cc64f041f570fc3aee4
Botan Abdullah <botan.b.abdullah@gmail.com>
2024-07-22 16:18:58 +0300

    Initial commit of weighbridge application

---
+++ weighbridge/weighbridge/doctype/loading_transaction/test_loading_transaction.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Botan Abdullah and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLoadingTransaction(FrappeTestCase):
+	pass
